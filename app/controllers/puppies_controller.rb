6d32114421b04d052721f7e8a17603f7f90e25f9
Matthias Meiklejohn <xcurtis.alexanderx@gmai.com>
2021-07-25 10:51:28 -0500

    make puppies create action and associated route

--- app/controllers/puppies_controller.rb
+++ app/controllers/puppies_controller.rb
@@ -3,4 +3,14 @@ class PuppiesController < ApplicationController
     puppies = Puppy.all
     render json: puppies.as_json
   end
+
+  def create
+    puppy = Puppy.new(
+      name: params[:name],
+      age: params[:age],
+      breed: params[:breed],
+    )
+    puppy.save
+    render json: puppy.as_json
+  end
 end
